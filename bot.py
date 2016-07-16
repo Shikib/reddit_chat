3f2fd92085f04af40d493dd7faa089a979f29ad8
shikib <mehrishikib@gmail.com>
2016-07-16 01:26:01 -0700

    implemented generation algorithm based on prompt

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import string
 import json
+import random
 
 from nltk import word_tokenize
 from nltk import pos_tag
@@ -14,6 +15,77 @@ class ContextAwareMarkovBot():
         self.size_reweight = int(size_reweight)
         self.punctuation_dataset = punctuation_dataset
         self.style_dataset = style_dataset 
+
+    def generate_message(self, prompt):
+        prompt = \
+            "".join([ch for ch in prompt if ch not in "'"])
+
+        words = word_tokenize(prompt)
+        chain = pos_tag(words)
+
+        while chain[-1][1] != '.':
+            potential_next_words = {}
+            for depth in range(1, self.ngram_len):
+                gram = chain[-depth:]
+
+                # Turn the gram into a hashable tuple to read from the style graph
+                words = " ".join([t[0] for t in gram])
+                tags = " ".join([t[1] for t in gram])
+                gram_tuple = (words,tags)
+
+                # If this chain of words has never occurred before, continue
+                if gram_tuple not in self.style_graph:
+                    continue
+
+                # Potential next words. Take the top twenty.
+                all_word_scores = self.style_graph[gram_tuple]
+                all_words = all_word_scores.keys()
+                top_words = \
+                    sorted(all_words, key=lambda w: -all_word_scores[w])[:20]
+                word_scores = {word: all_word_scores[word] for word in top_words}
+
+                # Use the part of speech tag information to determine the next POS 
+                if tags not in self.punctuation_graph:
+                    continue
+
+                all_pos_scores = self.punctuation_graph[tags]
+                all_pos = all_pos_scores.keys()
+                top_pos = sorted(all_pos, key=lambda p: -all_pos_scores[p])
+                pos_scores = {pos: all_pos_scores[pos] for pos in top_pos}
+
+                word_scores = \
+                    {word: 1.0*(word_scores[word]+pos_scores[word[1]])/2 
+                        for word in top_words if word[1] in pos_scores}
+
+                # Update master word list
+                for word,score in word_scores.items():
+                    if word not in potential_next_words:
+                        potential_next_words[word] = 0
+
+                    potential_next_words[word] = \
+                        max(potential_next_words[word],
+                            score * depth ** self.size_reweight)
+
+            # Only consider the top 50 words
+            all_words = potential_next_words.keys()
+            top_words = \
+                sorted(all_words, key=lambda w: -potential_next_words[w])[:100]
+            potential_next_words = \
+                {word: potential_next_words[word] for word in top_words}
+        
+            import pdb; pdb.set_trace()
+
+            # Choose the next word proportional to its score
+            scores_sum = sum(potential_next_words.values())
+            choice = random.random()
+
+            for word,score in potential_next_words.items():
+                choice -= score*1.0/scores_sum 
+                if choice <= 0:
+                    chain.append(word)
+                    break
+                    
+        return " ".join([word[0] for word in chain]) 
 
     def train_punctuation(self):
         # Initialize POS graph
@@ -106,4 +178,5 @@ if __name__ == '__main__':
                                 style_dataset='RC_2015-01')
     cmb.train_style()
     cmb.train_punctuation()
+    cmb.generate_message("I think")
     import pdb; pdb.set_trace()
