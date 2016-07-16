1ca5d8e84bdd84ae73c52f72a8ed336b0f9034d3
Nicolas Calle <ncalle@pinterest.com>
2016-07-16 03:21:03 -0700

    Chat UI primarily working

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, join_room, leave_room, send, emit
 import json
 #Access a file /static/foo.html with the path /foo.html
 app = Flask(__name__, static_url_path="")
@@ -7,9 +8,33 @@ app = Flask(__name__, static_url_path="")
  #Our routes
 @app.route('/')
 def root():
-    return "hello world"
+    return render_template("index.html")
+
+@app.route('/r/<subreddit>')
+@app.route('/r/<subreddit>/<username>')
+def get_subreddit(subreddit,username=None):
+    return render_template("index.html", sub=subreddit, username=username)
 
 
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+
+@socketio.on('connect')
+def handle_connection():
+    print("user connected!")
+
+@socketio.on('send_message')
+def handle_myevent(data):
+    emit("send_message",data["message"], room=data["room"], include_self=False)
+
+@socketio.on('join_room')
+def enter_user(data):
+    join_room(data["room"])
+    emit("send_message", data["username"] + " has joined the room.", room=data["room"], broadcase=True);
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    socketio.run(app, debug=True, port=8000)
